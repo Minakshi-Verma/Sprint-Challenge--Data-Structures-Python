a2ce7ed9835419c22f0fcab7f1ab35156f0ad36e
Minakshi Verma <mvedwan@gmail.com>
2020-06-12 13:56:43 -0400

    Run time is lowered!

--- names/names.py
+++ names/names.py
@@ -1,5 +1,51 @@
 import time
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+     
+        #if node is instantiated with None or node has no value
+        if self.value is None:
+            #set self.value to the value of inserted node
+            self.value = value
+        # if node already has a value(self.value) and inserted value >= existing node value
+        elif value >= self.value:
+            # if new value is greater...it will go on right, check if we already have a node on right
+            if self.right:
+            # if node.right is there, insert the value
+                self.right.insert(value)            
+            # if there is no right node, insert the new node with the value
+            else:
+                self.right = BSTNode(value)
+        #Now if the value< self.value, we insert the new value on left(check using ifel or just move to else block)
+        else:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)  
+    #-------------------------------------     
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value ==target:
+            return True
+
+        if self.value > target:
+            if self.left is not None:
+                return self.left.contains(target)
+
+        elif self.value< target:
+            if self.right:
+                return self.right.contains(target)
+        else:
+            return False
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,14 +59,31 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#Took 13.81200 sec with double nested loop
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:    
+#             duplicates.append(name_1)
+
+#Using BST tree
+#Create a BST tree of name_1 list using insert method
+# self.bst.insert(node)
+new_tree = BSTNode('tree')
+
+for name in names_1:
+    new_tree.insert(name)
+
+for name_2 in names_2:
+    if new_tree.contains(name):
+        duplicates.append(name) 
+
+#run time is now 0.22 second ( from 13+ seconds using nested loop)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,16 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity= capacity
-        self.storage = [None for i in range(capacity)]
+        self.storage = []
 
-    def append(self, item):
-        #append an item to the end of the buffer list
-        # self.storage.append(item)
-        # if len(self.storage) == self.capacity:
-        #     self.curr = 0
-        self.storage.pop(0)
-        self.storage[2: 4]
+    def append(self, item):            
+        # append the new item
         self.storage.append(item)
+        self.storage.pop(0)
+        self.storage.append(item)
+        # del self.storage[5:5]       
+        
+        # del self.storage[0:1]
 
     def get(self):
         return self.storage
